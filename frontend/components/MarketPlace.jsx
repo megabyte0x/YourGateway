339bd04bcd6405a59eec3be95f1ca35b2f8312ab
Pratham <prathamsinghal2003@gmail.com>
2023-04-18 21:46:09 +0530

    style: make marketplace responsive

--- frontend/components/MarketPlace.jsx
+++ frontend/components/MarketPlace.jsx
@@ -4,7 +4,7 @@ import MarketPlaceCard from "./MarketPlaceCard";
 const MarketPlace = () => {
   return (
     <div id="events" className="min-h-[60vh] bg-[#FFF253] ">
-      <div className="w-full max-w-[1280px] px-5 md:px-10 mx-auto py-4 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 lg:gap-10 grid-flow-row gap-5 ">
+      <div className="w-full max-w-[1280px] px-5 md:px-10 mx-auto py-5 md:py-10 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 lg:gap-10 grid-flow-row gap-5 ">
         <MarketPlaceCard />
         <MarketPlaceCard />
         <MarketPlaceCard />
--- frontend/components/MarketPlaceCard.jsx
+++ frontend/components/MarketPlaceCard.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 const MarketPlaceCard = () => {
   return (
-    <div className="flex min-h-fit justify-center hover:scale-105 duration-300 p-5 text-black max-md:flex-col max-md:mx-auto max-md:text-center">
+    <div className="flex min-h-fit justify-center hover:scale-105 duration-300 p-5 text-black max-md:flex-col max-md:mx-auto max-md:text-center mx-2">
       <div className="border border-black shadow-[8px_8px_0_#000000] min-w-fit bg-[#FFF89F]  rounded-md mx-4  flex justify-center content-center flex-col px-3 min-h-fit  max-md:mx-auto max-md:my-4 ">
-        <div className="bg-black rounded-lg h-[85%] w-full mx-auto mt-10 ">
+        <div className="bg-black rounded-lg h-[85%] w-full mx-auto mt-10 p-5 md:p-3">
           <Image
             src={"/hero-img.png"}
             width={200}
@@ -19,7 +19,7 @@ const MarketPlaceCard = () => {
             <h2 className="font-monument_bold text-black text-xs">
               Current Price
             </h2>
-            <h2 className="font-monument_bold text-[#7843E8] text-xl">7 ETH</h2>
+            <h2 className="font-monument_bold text-[#7843E8] text-xl">7 USD</h2>
           </section>
           <button className="bg-[#7843E8] rounded-3xl text-white px-3 my-1 hover:scale-95 text-xs gap-10 shadow-[5px_5px_0px_#000000] hover:shadow-[2px_2px_0px_#000000] duration-200 font-monument_reg border border-black  min-w-fit max-md:mx-auto max-md:px-5 ">
             Buy Now
@@ -32,8 +32,8 @@ const MarketPlaceCard = () => {
         <h2 className="font-monument_bold text-sm my-1">SubText</h2>
         <h2 className="font-monument_reg text-xs my-2 text-black/[0.65]">
           Lorem ing elit. Qui dicta minus molestiae vel beatae natus eveniet
-          ratione temporibus aperiam harum alias officiis assumenda officia
-          quibusdam deleniti eos cupidibu
+          ratione temporibus aperia massumenda officia quibusdam deleniti eos
+          cupidibu
         </h2>
         <Link
           href="/tickets/event-1"
