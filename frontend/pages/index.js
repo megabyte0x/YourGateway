684cc532c4f64406f4647659d8c0d8e53e06b197
Pratham <prathamsinghal2003@gmail.com>
2023-04-21 21:04:55 +0530

    style: revamp main page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,40 +1,29 @@
 import React from "react";
 import Head from "next/head";
+import Wrapper from "@/components/Wrapper";
+
 export default function Home() {
   return (
     <main>
       <Head>
         <title>FunTix</title>
       </Head>
-      <h2 className="font-inter text-7xl"> Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2>Hola</h2>
-      <h2 className="font-oswald">Hola</h2>
+      <div className="w-full my-5">
+        <Wrapper className="flex justify-around font-inter py-10">
+          <div className="basis-[40%] my-auto">
+            <h1 className="text-4xl font-semibold my-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+              non quiss
+            </h1>
+            <button className="bg-[#000000] my-5 rounded-xl text-white px-5 py-2 duration-300 hover:scale-95 min-w-fit w-[100px] ">
+              Get Early Access
+            </button>
+          </div>
+          <div className="basis-[40%]">
+            <img src="/demo-hero.jpg" alt="hero" className="rounded-lg" />
+          </div>
+        </Wrapper>
+      </div>
     </main>
   );
 }
